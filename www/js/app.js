58bc564edcc7c660752446658aa02fd2890aba3c
gloomyTofu <hello@cariephon.com>
2015-04-11 08:58:41 -0700

    Template Update

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+var snackApp = angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,50 +22,73 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-
-  .state('app', {
+    .state('app', {
     url: "/app",
     abstract: true,
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
+    })
+    .state('app.camera', {
+    url: "/camera",
     views: {
       'menuContent': {
-        templateUrl: "templates/search.html"
+        templateUrl: "templates/camera.html"
       }
     }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
+    })
+    .state('app.guides', {
+    url: "/guides",
     views: {
       'menuContent': {
-        templateUrl: "templates/browse.html"
+        templateUrl: "templates/guides.html",
+        controller: 'GuidesCtrl'
       }
     }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
+    })
+    .state('app.laos', {
+      url: "/laos",
       views: {
         'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
+          templateUrl: "templates/laos.html",
+          controller: 'LaosCtrl'
         }
       }
     })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
+    .state('app.single', {
+    url: "/laos/:dishesId",
     views: {
       'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
+        templateUrl: "templates/dishes.html",
+        controller: 'DishesCtrl'
       }
     }
-  });
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/guides');
+});
+
+
+// Cordova Camera
+snackApp.controller("SnackController", function($scope, $cordovaCamera) {
+    
+    $scope.takePicture = function() {
+        var options = { 
+            quality : 75, 
+            destinationType : Camera.DestinationType.DATA_URL, 
+            sourceType : Camera.PictureSourceType.CAMERA, 
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 300,
+            targetHeight: 300,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // An error occured. Show a message to the user
+        });
+    }
+ 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,16 +33,23 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('GuidesCtrl', function($scope) {
+  $scope.guides = [
+    { title: 'Laos', name: 'laos' },
+    { title: 'Thailand', name: 'thailand'  },
+    { title: 'Vietnam', name: 'vietnam'  }
+  ];
+})
+.controller('LaosCtrl', function($scope) {
+  $scope.laos = [
+    { title: 'Laap', id: 1 },
+    { title: 'Papaya Salad', id: 2 },
+    { title: 'Seafood Salad', id: 3 },
+    { title: 'Waffle', id: 4 },
+    { title: 'Curry Red Soup Noodle', id: 5 },
+    { title: 'Buffalo Jerky', id: 6 }
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('DishesCtrl', function($scope, $stateParams) {
 });
